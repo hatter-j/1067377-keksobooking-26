3ea9a54837b02637996ff3517f59880d341a1c33
hatter-j <hatterjulia@mail.ru>
2022-07-18 02:42:12 +0300

    Отрисуй меня полностью (часть 1) ещё правки

--- js/card.js
+++ js/card.js
@@ -1,7 +1,6 @@
 import {createAdverts} from './data.js';
 
 const mapCanvasElement = document.querySelector('.map__canvas');
-//const popupElement = document.querySelector('.popup');
 const advertCardTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
@@ -20,27 +19,23 @@ const simularAdvertsFragment = document.createDocumentFragment();
 
 simularAdverts.forEach(({offer, author}) => {
   const advertElement = advertCardTemplate.cloneNode(true);
-  advertElement.querySelector('.popup__title').textContent = offer.title ? offer.title: '';
-  advertElement.querySelector('.popup__text--address').textContent = offer.address ? offer.address: '';
-  advertElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь` ? offer.price: ''; /* у цены пропало ₽/ночь */
-  advertElement.querySelector('.popup__type').textContent = advertTypeValue[offer.type]; /*? offer.type: ''; не работает тернарный оператор*/
-  // if (offer.type === 'flat') {
-  //   advertElement.querySelector('.popup__type').textContent = 'Квартира';
-  // } if (offer.type === 'bungalow') {
-  //   advertElement.querySelector('.popup__type').textContent = 'Бунгало';
-  // } if (offer.type === 'house') {
-  //   advertElement.querySelector('.popup__type').textContent = 'Дом';
-  // } if (offer.type === 'palace') {
-  //   advertElement.querySelector('.popup__type').textContent = 'Дворец';
-  // } if (offer.type === 'hotel') {
-  //   advertElement.querySelector('.popup__type').textContent = 'Отель';
-  // }
-  advertElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`; /* добавить тернариник */
-  advertElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`; /* добавить тернариник */
-  advertElement.querySelector('.popup__features').textContent = offer.features ? offer.features: '';
-  advertElement.querySelector('.popup__description').textContent = offer.description ? offer.description: '';
-  //advertElement.querySelector('.popup__photos').src = offer.photos;
-  advertElement.querySelector('.popup__avatar').src = author.avatar ? author.avatar: '';
+  advertElement.querySelector('.popup__title').textContent = offer.title ? offer.title : '';
+  advertElement.querySelector('.popup__text--address').textContent = offer.address ? offer.address : '';
+  advertElement.querySelector('.popup__text--price').textContent = offer.price ? `${offer.price} ₽/ночь` : '';
+  advertElement.querySelector('.popup__type').textContent = offer.type ? advertTypeValue[offer.type] : '';
+  advertElement.querySelector('.popup__text--capacity').textContent = offer.rooms && offer.guests ? `${offer.rooms} комнаты для ${offer.guests} гостей` : '';
+  advertElement.querySelector('.popup__text--time').textContent = offer.checkin && offer.checkout ? `Заезд после ${offer.checkin}, выезд до ${offer.checkout}` : '';
+  advertElement.querySelector('.popup__features').textContent = offer.features ? offer.features : '';
+  advertElement.querySelector('.popup__description').textContent = offer.description ? offer.description : '';
+  const advertPhotosContainerElement = advertElement.querySelector('.popup__photos');
+  const advertPhotoElement = advertPhotosContainerElement.querySelector('.popup__photo');
+  offer.photos.forEach((el) => {
+    const advertPhotoCloneElement = advertPhotoElement.cloneNode(true);
+    advertPhotoCloneElement.src = el;
+    advertPhotosContainerElement.append(advertPhotoCloneElement);
+  });
+  advertPhotoElement.style.display = 'none';
+  advertElement.querySelector('.popup__avatar').src = author.avatar ? author.avatar : '';
 
   simularAdvertsFragment.append(advertElement);
 });
