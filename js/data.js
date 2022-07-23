51b9ff594cf4c96c14356ac5723b096f61b2d8d0
hatter-j <hatterjulia@mail.ru>
2022-07-23 23:05:04 +0300

    Правда или действие (часть 2

--- js/data.js
+++ js/data.js
@@ -83,4 +83,27 @@ const createAdverts = (advertsQuantity) => {
   return adverts;
 };
 
-export {createAdverts};
+const ADVERT_TYPE_VALUE = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
+const ROOM_OPTION = {
+  '1' : ['1'],
+  '2' : ['1', '2'],
+  '3' : ['1', '2', '3'],
+  '100' : ['0'],
+};
+
+const MIN_PRICE = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+
+export {createAdverts, ADVERT_TYPE_VALUE, ROOM_OPTION, MIN_PRICE};
--- js/form-validation.js
+++ js/form-validation.js
@@ -1,4 +1,5 @@
 import {formElement} from './form-status.js';
+import {ROOM_OPTION, MIN_PRICE} from './data.js';
 
 const pristine = new Pristine(formElement, {
   classTo: 'ad-form__element',
@@ -8,20 +9,44 @@ const pristine = new Pristine(formElement, {
 
 const validRoomsElement = formElement.querySelector('#room_number');
 const validCapacityElement = formElement.querySelector('#capacity');
-
-const ROOM_OPTION = {
-  '1' : ['1'],
-  '2' : ['1', '2'],
-  '3' : ['1', '2', '3'],
-  '100' : ['0'],
-};
+const validTypeElement = formElement.querySelector('#type');
+const validPriceElement = formElement.querySelector('#price');
+const validCheckInElement = formElement.querySelector('#timein');
+const validCheckOutElement = formElement.querySelector('#timeout');
 
 const getValidRoom = () => ROOM_OPTION[validRoomsElement.value].includes(validCapacityElement.value);
-
 const getRoomOptionErrorMessage = () => `Выбрано: ${validRoomsElement.value} комнат. Выберите другое количество мест, например: ${ROOM_OPTION[validRoomsElement.value].join(' или ')}`;
+
+const getValidPrice = () => validPriceElement >= MIN_PRICE[validTypeElement.value];
+const getMinPriceErrorMessage = () => `Цена не может быть меньше: ${MIN_PRICE[validTypeElement.value]}`;
+
+validRoomsElement.addEventListener('change', () => {
+  pristine.validate([validRoomsElement, validCapacityElement]);
+});
+validCapacityElement.addEventListener('change', () => {
+  pristine.validate([validRoomsElement, validCapacityElement]);
+});
+
+validTypeElement.addEventListener('change', () => {
+  validPriceElement.placeholder = MIN_PRICE[validTypeElement.value];
+  validPriceElement.min = MIN_PRICE[validTypeElement.value];
+  pristine.validate([validTypeElement, validPriceElement]);
+});
+validPriceElement.addEventListener('change', () => {
+  pristine.validate([validTypeElement, validPriceElement]);
+});
+
+validCheckInElement.addEventListener('change', () => {
+  validCheckOutElement.value = validCheckInElement.value;
+});
+validCheckOutElement.addEventListener('change', () => {
+  validCheckInElement.value = validCheckOutElement.value;
+});
 
 pristine.addValidator(validRoomsElement, getValidRoom, getRoomOptionErrorMessage);
 pristine.addValidator(validCapacityElement, getValidRoom, getRoomOptionErrorMessage);
+
+pristine.addValidator(validTypeElement, getValidPrice, getMinPriceErrorMessage);
 
 formElement.addEventListener('submit', (evt) => {
   evt.preventDefault();
