d8948603f293e948dd0be3ab316818dff1b86e0b
hatter-j <hatterjulia@mail.ru>
2022-07-26 22:12:22 +0300

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-import './form-validation.js';
-import './map.js';
+import {setUserFormSubmit} from './form-validation.js';
+import {createMarker} from './map.js';
+import {getData} from './fetch-data.js';
+import {showErrorLoadData} from './util.js';
+
+const advertsQuantity = 10;
+
+getData(
+  (adverts) => createMarker(adverts.slice(0, advertsQuantity)),
+  () => showErrorLoadData('При загрузке данных с сервера произошла ошибка!')
+);
+
+setUserFormSubmit();
